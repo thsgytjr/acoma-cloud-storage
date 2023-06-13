b31c7615bdf7ae04e97086be53f0c2ffa730e5f9
David Son <davidson@davids-air.lan>
2023-06-12 22:36:34 -0500

    spring initializr

---
+++ src/main/java/com/example/acomacloudstorageservice/AcomaCloudStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.acomacloudstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcomaCloudStorageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcomaCloudStorageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/acomacloudstorageservice/AcomaCloudStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acomacloudstorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcomaCloudStorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
